a0f7959fcac87a44fa7dd2b28686f36df6cbf9b3
MOHAMEDAfrath <afrathismath@gmail.com>
2021-07-25 20:46:12 +0530

    [Afrath]Updated the file to check whether the file exists

---
+++ FileIOOperations/FileOperations.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIOOperations
+{
+    class FileOperations
+    {
+        public static void FileOperationsDemo()
+        {
+            string path = @"C:\Users\afrat\source\repos\FileIOOperations\FileIOOperations\FileIO.txt";
+            if (File.Exists(path))
+            {
+                Console.WriteLine("File Exists");
+            }
+        }
+    }
+}
--- FileIOOperations/Program.cs
+++ FileIOOperations/Program.cs
@@ -7,6 +7,7 @@ namespace FileIOOperations
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            FileOperations.FileOperationsDemo();
         }
     }
 }
